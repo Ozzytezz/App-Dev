7f090b7ff329cd9305c4efb91eef5cadb9f67a2a
Ozzytezz <145788386+Ozzytezz@users.noreply.github.com>
2024-08-07 08:19:40 +0800

    Add files via upload

---
+++ SASS Template/dist/js/burgir.js
@@ -0,0 +1,16 @@
+const burgirBtn = document.querySelector('.burgir');
+const headerNav = document.querySelector('.header__nav');
+burgirBtn.addEventListener("click", () =>{
+    burgirBtn.classList.toggle("open");
+    headerNav.classList.toggle("open");
+})
+
+
+
+
+
+
+
+
+
+
